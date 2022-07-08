a2a8199281352e02d34b5b3b3082bfb3a8244546
Damian Cybiak <cybiakdamian@gmail.com>
2022-07-08 10:07:35 +0200

    wyszukiwanie po kliknięciu w enter

--- src/components/Header/Searchbar/Searchbar.js
+++ src/components/Header/Searchbar/Searchbar.js
@@ -9,16 +9,25 @@ function SearchBar() {
         setTerm(e.target.value);
     }
 
+    const onKeyDownHandler = (e) => {
+        //e.key === "Enter" && search() to samo co if na dole
+        if (e.key === "Enter") {
+            search();
+        }
+    }
+
     return (
         <div className="d-flex">
             <input value={term}
                    onChange={updateTerm}
+                   onKeyDown={onKeyDownHandler}
                    type="text"
                    className='form-control'
                    placeholder="Szukaj..."/>
 
             <button onClick={search}
-                    className='btn btn-primary'>Szukaj</button>
+                    className='btn btn-primary'>Szukaj
+            </button>
         </div>
     )
 }
